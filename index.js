dac2ad055853ba154a1bd98add31f887a0199982
nihadkuluzade <nihadquluzade2001@gmail.com>
2024-09-02 09:24:25 +0400

    2ci task

---
+++ index.js
@@ -0,0 +1,46 @@
+
+function numberToString(num) {
+    return num.toString();
+}
+
+console.log(numberToString(123));
+
+
+
+
+
+
+
+function findMaxValue(numbers) {
+    return Math.max(...numbers);
+}
+
+console.log(findMaxValue([3, 18, 56, 15]));
+
+
+
+
+
+
+
+function roundNumber(num) {
+    return Math.round(num);
+}
+
+console.log(roundNumber(4.7));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
